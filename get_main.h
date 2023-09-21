940ffdd7fb5d8a7fea3f9c9278a21e6b2fff2416
Eugo22 <eugenecodez@gmail.com>
2023-09-21 02:41:40 -0700

    fix error

---
+++ get_main.h
@@ -0,0 +1,9 @@
+#ifndef GET_MAIN_H
+#define GET_MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define BUFFER_SIZE 1024
+ssize_t my_getline(char **lineptr, size_t *n, int fd);
+
+#endif
