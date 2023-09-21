7d68204e43e373ffc0ff26a5c3cd5ef7246eaa09
Eugo22 <eugenecodez@gmail.com>
2023-09-20 18:59:39 -0700

    task 7

---
+++ get_main.c
@@ -0,0 +1,18 @@
+#include "get_main.h"
+/**
+ * Entry - point of the program
+ *
+ */
+int main()
+{
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t read;
+
+        while ((read = my_getline(&line, &len, STDIN_FILENO)) != -1)
+        {
+                printf("%s", line);
+        }
+        free(line);
+        return (0);
+}
